062b7feaf77339983a704af94d507d9678e582f3
ststeiger <quandary82@hailmail.net>
2015-03-23 12:10:45 +0100

    NuGet

--- ProtectedDownloadForm/Properties/AssemblyInfo.cs
+++ ProtectedDownloadForm/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2015.3.23.1207")]
-[assembly: AssemblyFileVersion("2015.3.23.1207")]
+[assembly: AssemblyVersion("2015.3.23.1210")]
+[assembly: AssemblyFileVersion("2015.3.23.1210")]
